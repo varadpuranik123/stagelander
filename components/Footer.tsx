c4012ca5f7ac533942f9c324881bf65310179876
vdpop <varadpuranik123@gmail.com>
2025-11-26 00:11:40 +0530

    Footer logo changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='max-w-screen-2xl relative mx-auto w-full h-fit flex flex-col items-center justify-between md:px-24 px-6 md:py-20 py-12 bg-linear-to-b from-[#121212]/0 via-[#121212]/0 to-[#F59E0B]/15 '>
             <div className="w-full h-fit flex md:flex-row flex-col gap-y-8 md:gap-y-0 items-start justify-between">
                 <div className=" h-fit space-y-3 ">
-                    <Image src="/logo.svg" alt="logo" width={100} height={100} className="w-42" />
+                    <Image src="/Logo.svg" alt="logo" width={100} height={100} className="w-42" />
                     <p className="text-lg text-white/60 max-w-70 leading-tight tracking-tight font-medium ">Create stunning visual designs with Stage fully in browser canvas editor</p>
                     <p className="text-lg text-white/60 max-w-70 font-bold leading-tight tracking-tight  ">&copy; 2025 Stage</p>
                 </div>
